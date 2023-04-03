3feb3f64c4dabfd4c9218e514641e5be07d98226
Perc <carlpericles18@gmail.com>
2023-04-03 21:26:56 +0900

    Added dollar sign

--- Expense/src/Component/Spending.jsx
+++ Expense/src/Component/Spending.jsx
@@ -11,13 +11,13 @@ export default function Spending(){
             <div>
                 <h2 className="days"> Spending - Last 7 days</h2>
                 <div className="WeeksChart">
-                    <div className="Box1 Box"><div className="price">{dataPrice[0].amount}</div><p>{dataPrice[0].day}</p></div>
-                    <div className="Box2 Box"><div className="price">{dataPrice[1].amount}</div><p>{dataPrice[1].day}</p></div>  
-                    <div className="Box3 Box"><div className="price">{dataPrice[2].amount}</div><p>{dataPrice[2].day}</p></div>  
-                    <div className="Box4 Box"><div className="price">{dataPrice[3].amount}</div><p>{dataPrice[3].day}</p></div>   
-                    <div className="Box5 Box"><div className="price">{dataPrice[4].amount}</div><p>{dataPrice[4].day}</p></div>   
-                    <div className="Box6 Box"><div className="price">{dataPrice[5].amount}</div><p>{dataPrice[5].day}</p></div>    
-                    <div className="Box7 Box"><div className="price">{dataPrice[6].amount}</div><p>{dataPrice[6].day}</p></div>   
+                    <div className="Box1 Box"><div className="price">${dataPrice[0].amount}</div><p>{dataPrice[0].day}</p></div>
+                    <div className="Box2 Box"><div className="price">${dataPrice[1].amount}</div><p>{dataPrice[1].day}</p></div>  
+                    <div className="Box3 Box"><div className="price">${dataPrice[2].amount}</div><p>{dataPrice[2].day}</p></div>  
+                    <div className="Box4 Box"><div className="price">${dataPrice[3].amount}</div><p>{dataPrice[3].day}</p></div>   
+                    <div className="Box5 Box"><div className="price">${dataPrice[4].amount}</div><p>{dataPrice[4].day}</p></div>   
+                    <div className="Box6 Box"><div className="price">${dataPrice[5].amount}</div><p>{dataPrice[5].day}</p></div>    
+                    <div className="Box7 Box"><div className="price">${dataPrice[6].amount}</div><p>{dataPrice[6].day}</p></div>   
                 </div>
                 <hr></hr>
                 <div className="lastBox">
